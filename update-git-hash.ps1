1d3b0bf45f467f013e8df5567c3d9bb2b45d48ee
David L. Berger <dberger@ComputersByDavid.com>
2026-04-13 05:43:08 -0700

    Hopefully working correctly

--- update-git-hash.ps1
+++ update-git-hash.ps1
@@ -1,18 +1,21 @@
-# update-GitHashh.ps1
+# update-git-hash.ps1
 
 # Get commit hash (short form)
 $commitHash = git rev-parse --short HEAD
 
-# Get list of files changed in last commit (not deleted)
+# Get files changed in the latest commit (ignore deleted files)
 $changedFiles = git diff-tree --no-commit-id --name-only -r HEAD | Where-Object {
     Test-Path $_
 }
+
+$commitAmended = $false
 
 foreach ($file in $changedFiles) {
     $lines = Get-Content $file
     $lineFound = $false
     $newLines = $lines | ForEach-Object {
-        if (-not $lineFound -and $_ -match '^\$GitHash\s*=\s*.*$') {
+        # Match only lines at the beginning: $GitHash = "<something>"
+        if (-not $lineFound -and $_ -match '^\$GitHash\s*=\s*".*"$') {
             $lineFound = $true
             '$GitHash = "' + $commitHash + '"'
         } else {
@@ -21,13 +24,11 @@ foreach ($file in $changedFiles) {
     }
     if ($lineFound) {
         Set-Content -Path $file -Value $newLines
-        # Stage and amend ONLY if something changed
         git add $file
         $commitAmended = $true
     }
 }
 
-# If any file was amended, amend the last commit
 if ($commitAmended) {
     git commit --amend --no-edit
 }
